139d60456e8199f0a3c25a91e87b8675a5e22015
jfmendez11 <jf.mendez11@uniandes.edu.co>
2020-06-11 12:47:04 -0500

    Concenntration (Stanford CS193P iOS 11 Swift 4)

---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,43 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Juan Felipe Méndez on 8/06/20.
+//  Copyright © 2020 Juan Felipe Méndez. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMathced {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMathced = true
+                    cards[index].isMathced = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+    }
+}
