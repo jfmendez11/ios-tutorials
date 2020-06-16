5b9cc49db46eab9580abcf73caa81d4074195200
jfmendez11 <jf.mendez11@uniandes.edu.co>
2020-06-15 21:35:52 -0500

    Drawing

---
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Juan Felipe Méndez on 15/06/20.
+//  Copyright © 2020 Juan Felipe Méndez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
+
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
