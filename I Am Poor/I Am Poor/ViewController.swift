3c2c3ecac33353fcbb7fd4967bf4a0ebf0083726
jfmendez11 <jf.mendez11@uniandes.edu.co>
2020-06-20 14:33:17 -0500

    Mini projects from Udemy's Bootcamp

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Juan Felipe Méndez on 19/06/20.
+//  Copyright © 2020 Juan Felipe Méndez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
